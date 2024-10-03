63f403a26b60f0eac9e6d3ba79df8c2619fe355c
Diego Silva <144260639+devdiego365@users.noreply.github.com>
2024-10-03 16:10:04 -0300

    Encapsulamento
    .

---
+++ Pessoa.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visibilidade
+{
+    internal class Pessoa
+    {
+        private string Nome;
+        private int Idade;
+        private string Telefone;
+        private string Endereco;
+        private string Email;
+
+        public string getNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            if (String.IsNullOrWhiteSpace(nome) == false)
+            {
+
+                 Nome = nome;
+            }
+            else
+            {
+                Console.WriteLine("Nome não pode estar vazio");
+            }
+        }
+        public int getIdade()
+        {
+            return Idade;
+        }
+        public void setIdade(int idade)
+        {
+            if (idade > 0)
+            {
+                 Idade = idade;
+            }
+            else
+            {
+                Console.WriteLine("Idade invalida");
+            }
+        }
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+        public void setTelefone(string telefone)
+        {
+            if (String.IsNullOrWhiteSpace(telefone) == false)
+            {
+                Telefone = telefone;
+            }
+            else
+            {
+                Console.WriteLine("Telefone não pode ficar em branco");
+            }
+        }
+        public string getEmail()
+        {
+            return Email;
+        }
+        public void setEmail(string email)
+        {   if (String.IsNullOrWhiteSpace(email) == false)
+            {
+                 Email = email;
+            }
+            else
+            {
+                Console.WriteLine("Email não pode estar vazio");
+            }
+        }
+        public string getEndereco()
+        {
+            return Endereco;
+        }
+        public void setEndereco(string endereco)
+        {
+            if (String.IsNullOrWhiteSpace(endereco) == false)
+            {
+                Endereco = endereco;
+            }
+            else
+            {
+                Console.WriteLine("Endereço não pode ficar em branco");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Visibilidade;
+
+
+Pessoa p =  new Pessoa();
+p.setNome("Diego");
+Console.WriteLine(p.getNome());
+
+
+p.setIdade(8);
+Console.WriteLine(p.getIdade());
+
+p.setTelefone("81993966901");
+Console.WriteLine(p.getTelefone());
+
+p.setEmail("diegodasilva365@gmail.com");
+Console.WriteLine(p.getEmail());
+
+p.setEndereco("Cabo");
+Console.WriteLine(p.getEndereco());
+
+Console.ReadKey();
